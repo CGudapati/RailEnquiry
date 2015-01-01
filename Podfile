2fc60d01c735302c5a26ffb2497d7c782c6d2249
Gudapati Naga Venkata Chaitanya <cgudapati@outlook.com>
2015-01-01 19:52:29 +0530

    PNR working

---
+++ Podfile
@@ -0,0 +1,3 @@
+source 'https://github.com/CocoaPods/Specs.git'
+platform :osx, '10.8'
+pod 'AFNetworking', '~> 2.4'
--- RailEnquiry/main.m
+++ RailEnquiry/main.m
@@ -7,11 +7,96 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <AFNetworking/AFNetworking.h>
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        BOOL __block done = NO;
+        
+        NSRunLoop *runLoop = [NSRunLoop currentRunLoop];
+        NSString * BaseURLString = @"http://api.erail.in/pnr?key=599c66c2-493d-47e6-abe9-af2a364ae81b&pnr=2629007687";
+
+        
+        NSURL *url = [NSURL URLWithString:BaseURLString];
+        NSURLRequest *request = [NSURLRequest requestWithURL:url];
+        
+        AFHTTPRequestOperation *operation = [[AFHTTPRequestOperation alloc] initWithRequest:request];
+        operation.responseSerializer = [AFJSONResponseSerializer serializer];
+        operation.responseSerializer.acceptableContentTypes = [NSSet setWithObject:@"text/html"];
+        
+        [operation setCompletionBlockWithSuccess:^(AFHTTPRequestOperation *operation, id responseObject) {
+            NSDictionary *statusDict = (NSDictionary *) responseObject;
+            NSLog(@"%@", statusDict);
+            
+        
+            NSDictionary *resultDictionary = [statusDict objectForKey:@"result"];
+            NSDictionary *passDict = [resultDictionary objectForKey:@"passengers"];
+            NSLog(@"%@", passDict);
+            
+            NSUInteger numberofPassengers = [passDict count];
+            NSMutableArray *arrayOfPassengers = [[NSMutableArray alloc] init];
+            for (int i = 1; i<=numberofPassengers; ++i) {
+                NSMutableString *baseString = [NSMutableString stringWithString:@"Passenger"];
+                [baseString appendString:[NSString stringWithFormat:@"%i", i]];
+                [arrayOfPassengers addObject:baseString];
+            }
+            NSMutableArray *arrayofStatuses = [[NSMutableArray alloc] init];
+            for (NSString *random in passDict) {
+                [arrayofStatuses addObject:random];
+            }
+            
+            
+            NSDictionary *passengerDictionary = [[NSDictionary alloc] initWithObjects:arrayofStatuses forKeys:arrayOfPassengers];
+            NSLog(@"%@", passengerDictionary);
+            
+            NSString *class;
+            class = [resultDictionary objectForKey:@"cls"];
+            NSString *eticket;
+            eticket =  [resultDictionary objectForKey:@"eticket"];
+            NSString *destination;
+            destination = [resultDictionary objectForKey:@"to"];
+            NSString *pnr;
+            pnr = [resultDictionary objectForKey:@"pnr"];
+            NSString *journeyDate;
+            journeyDate = [resultDictionary objectForKey:@"journey"];
+            NSString *boardingStation;
+            boardingStation = [resultDictionary objectForKey:@"brdg"];
+            NSString *chartingStatus;
+            chartingStatus = [resultDictionary objectForKey:@"chart"];
+            NSString *bookedFrom;
+            bookedFrom = [resultDictionary objectForKey:@"from"];
+            NSString *trainNo;
+            trainNo = [resultDictionary objectForKey:@"trainno"];
+            NSString *trainName;
+            trainName = [resultDictionary objectForKey:@"name"];
+            NSLog(@"********************************************");
+            NSLog(@"PNR              : %@", pnr);
+            NSLog(@"Booking Station  : %@", bookedFrom);
+            NSLog(@"Boarding Station : %@", boardingStation);
+            NSLog(@"Destination      : %@", destination);
+            NSLog(@"Journey Date     : %@", journeyDate);
+            NSLog(@"Class            : %@", class);
+            NSLog(@"Train No         : %@", trainNo);
+            NSLog(@"Train Name       : %@", trainName);
+            NSLog(@"Charting Status  : %@", chartingStatus);
+            NSLog(@"Booking Status   : %@", passengerDictionary);
+            NSLog(@"********************************************");
+            
+//            NSLog(@"YES");
+            done = YES;
+
+            
+        } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+            NSLog(@"NO!");
+            done = YES;
+            
+        }];
+        [operation start];
+        
+        
+        while (!done && [runLoop runMode:NSDefaultRunLoopMode beforeDate:[NSDate distantFuture]]) {
+            // this is intentionally blank
+        }
     }
     return 0;
 }
