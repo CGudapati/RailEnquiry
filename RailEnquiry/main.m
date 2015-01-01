3339804dceba459aab21173981e158103f8c815e
Gudapati Naga Venkata Chaitanya <cgudapati@outlook.com>
2015-01-01 11:30:52 +0530

    Initial Commit

---
+++ RailEnquiry/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  RailEnquiry
+//
+//  Created by Venkat on 01/01/15.
+//  Copyright (c) 2015 Gudapati Naga Venkata Chaitanya. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
